788a7fcccb4cd4cff9681b95f5f02609dcb7fb29
Adrian Mostowski <adaxelx@o2.pl>
2019-03-25 22:33:29 +0100

    add css to form in contact.css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -23,17 +23,17 @@ class Contact extends React.Component {
                 <h1 className="title">Kontakt</h1>
                 <p className='content'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                 <p className='content'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                <form>
-                    <label htmlFor="user">
-                        Twoje imię:
-                        <input type="text" id="user" name="username" value={this.state.username} onChange={this.handleChange}></input>
+                <form className="contactForm">
+                    <label className='name' htmlFor="user">
+                        <p>Imię</p>
+                        <input placeholder='Imię' type="text" id="user" name="username" value={this.state.username} onChange={this.handleChange}></input>
                     </label>
-                    <label htmlFor="email">
-                        Twój mail:
-                        <input type="text" id="email" name="email" value={this.state.email} onChange={this.handleChange}></input>
+                    <label className='email' htmlFor="email">
+                        <p>Email</p>
+                        <input placeholder='Email' type="text" id="email" name="email" value={this.state.email} onChange={this.handleChange}></input>
                     </label>
-                    <label htmlFor="message">
-                        Twoja wiadomość
+                    <label className='message' htmlFor="message">
+                        <p>Wiadomość</p>
                         <textarea id="message" name="message" value={this.state.message} onChange={this.handleChange}></textarea>
                     </label>
                 </form>
