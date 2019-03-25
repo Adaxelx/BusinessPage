96b78074b89e47f9166ca8465c315a47f03e210a
Adrian Mostowski <adaxelx@o2.pl>
2019-03-25 21:58:01 +0100

    add form to contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,13 +3,40 @@ import React from 'react';
 import '../css/Contact.css';
 
 class Contact extends React.Component {
-    state = {  }
+    state = { 
+        username: '',
+        email: '',
+        message:'',
+     }
+
+     handleChange = (e) =>{
+        const name = e.target.name;
+        const value = e.target.value;
+        this.setState({
+            [name]: value,
+        })
+     }
+
     render() { 
         return ( 
             <section className='contact'>
                 <h1 className="title">Kontakt</h1>
                 <p className='content'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
                 <p className='content'>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+                <form>
+                    <label htmlFor="user">
+                        Twoje imię:
+                        <input type="text" id="user" name="username" value={this.state.username} onChange={this.handleChange}></input>
+                    </label>
+                    <label htmlFor="email">
+                        Twój mail:
+                        <input type="text" id="email" name="email" value={this.state.email} onChange={this.handleChange}></input>
+                    </label>
+                    <label htmlFor="message">
+                        Twoja wiadomość
+                        <textarea id="message" name="message" value={this.state.message} onChange={this.handleChange}></textarea>
+                    </label>
+                </form>
             </section>
          );
     }
