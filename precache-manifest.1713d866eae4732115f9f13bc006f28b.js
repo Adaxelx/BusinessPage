8d5908864b0cf38562af6cb36ae07a4d38f10343
Adrian Mostowski <adaxelx@o2.pl>
2019-03-27 21:46:27 +0100

    Updates

---
+++ precache-manifest.1713d866eae4732115f9f13bc006f28b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "53016398a6e87126f79338902677e072",
+    "url": "/react-git-first-project/static/media/img.53016398.jpg"
+  },
+  {
+    "revision": "be43563195ed65d7d4e7cce3fd9d89d3",
+    "url": "/react-git-first-project/static/media/hong-kong.be435631.jpg"
+  },
+  {
+    "revision": "bfcbf6e3e6abd49f36ac071b84200de5",
+    "url": "/react-git-first-project/static/media/clouds.bfcbf6e3.jpg"
+  },
+  {
+    "revision": "430fc2b51c872a60e9c9",
+    "url": "/react-git-first-project/static/js/runtime~main.5b1503ad.js"
+  },
+  {
+    "revision": "8473333f31d40ed65379",
+    "url": "/react-git-first-project/static/js/main.bbbf4b18.chunk.js"
+  },
+  {
+    "revision": "9026dd6dfda8f5724dab",
+    "url": "/react-git-first-project/static/js/2.e7cd2972.chunk.js"
+  },
+  {
+    "revision": "8473333f31d40ed65379",
+    "url": "/react-git-first-project/static/css/main.a99abb9b.chunk.css"
+  },
+  {
+    "revision": "077ad2ae8b84a7db2dcaf6f7cc69608e",
+    "url": "/react-git-first-project/index.html"
+  }
+];
