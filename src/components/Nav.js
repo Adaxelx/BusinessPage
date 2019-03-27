e4af973bab5aa73919e396b4338c654551590ba7
Adrian Mostowski <adaxelx@o2.pl>
2019-03-27 22:05:01 +0100

    add timeout to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -8,6 +8,7 @@ class Nav extends React.Component {
     state = { 
         active: false,
         activeNav: false,
+        flag: true,
      }
 
      changeStateNav = ()=>{
@@ -16,10 +17,18 @@ class Nav extends React.Component {
         })
      }
 
+     changeFlag = ()=>{
+         this.setState({
+             flag: true,
+         })
+     }
+
      handleClick = ()=>{
          this.setState({
+             flag: false,
              active: !this.state.active,
          })
+         setTimeout(this.changeFlag,1500)
          if(this.state.activeNav){
             setTimeout(this.changeStateNav, 1000)
         }else{
@@ -57,7 +66,7 @@ class Nav extends React.Component {
 
         return (
         <>
-        <button ref={this.nav} className='navBtn' onClick={this.handleClick}>
+        <button ref={this.nav} className='navBtn' onClick={this.state.flag ? this.handleClick : null}>
             <span className={activeNav ? 'top bar active' : 'top bar'}></span>
             <span className={activeNav ? 'mid bar active' : 'mid bar'}></span>
             <span className={activeNav ? 'bot bar active' : 'bot bar'}></span>
@@ -66,9 +75,9 @@ class Nav extends React.Component {
         <nav className={activeNav ? 'active' : ''}>
             <ul>
                 <NavLink to='/' onClick={this.handleClick}><li><span className={active ? 'number active' : 'number'}>01</span><span className={active ? 'txt active' : 'txt'}>Start</span></li></NavLink>
-                <NavLink to='/offer' onClick={this.handleClick}><li><span className={active ? 'number active' : 'number'}>02</span><span className={active ? 'txt active' : 'txt'}>Oferta</span></li></NavLink>
-                <NavLink to='/place' onClick={this.handleClick}><li><span className={active ? 'number active' : 'number'}>03</span><span className={active ? 'txt active' : 'txt'}>Gdzie nas znajdziesz</span></li></NavLink>
-                <NavLink to='/contact' onClick={this.handleClick}><li><span className={active ? 'number active' : 'number'}>04</span><span className={active ? 'txt active' : 'txt'}>Kontakt</span></li></NavLink>
+                <NavLink to='/offer' onClick={this.state.flag ? this.handleClick : null}><li><span className={active ? 'number active' : 'number'}>02</span><span className={active ? 'txt active' : 'txt'}>Oferta</span></li></NavLink>
+                <NavLink to='/place' onClick={this.state.flag ? this.handleClick : null}><li><span className={active ? 'number active' : 'number'}>03</span><span className={active ? 'txt active' : 'txt'}>Gdzie nas znajdziesz</span></li></NavLink>
+                <NavLink to='/contact' onClick={this.state.flag ? this.handleClick : null}><li><span className={active ? 'number active' : 'number'}>04</span><span className={active ? 'txt active' : 'txt'}>Kontakt</span></li></NavLink>
             </ul>
         </nav>
         </>
