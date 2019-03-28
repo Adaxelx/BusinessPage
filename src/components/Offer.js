f9673a9aa68d90bd9413ecea81a08972f7ebb782
Adrian Mostowski <adaxelx@o2.pl>
2019-03-28 14:57:43 +0100

    add offer articles

--- src/components/Offer.js
+++ src/components/Offer.js
@@ -8,6 +8,21 @@ class Offer extends React.Component {
         return ( 
             <section className='offer'>
                 <p className='title' data-text="Oferta">Oferta</p>
+                <article>
+                    <i className="fas fa-users icon"></i>
+                    <p className="titleArt">Lorem ipsum</p>
+                    <p className="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam alias maxime illum omnis praesentium cum delectus laboriosam. Repellendus non officia quaerat rerum culpa debitis, dolorem sed perferendis officiis corrupti minus!</p>
+                </article>
+                <article>
+                    <i className="fas fa-users icon"></i>
+                    <p className="titleArt">Lorem ipsum</p>
+                    <p className="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam alias maxime illum omnis praesentium cum delectus laboriosam. Repellendus non officia quaerat rerum culpa debitis, dolorem sed perferendis officiis corrupti minus!</p>
+                </article>
+                <article>
+                    <i className="fas fa-users icon"></i>
+                    <p className="titleArt">Lorem ipsum</p>
+                    <p className="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam alias maxime illum omnis praesentium cum delectus laboriosam. Repellendus non officia quaerat rerum culpa debitis, dolorem sed perferendis officiis corrupti minus!</p>
+                </article>
             </section>
          );
     }
