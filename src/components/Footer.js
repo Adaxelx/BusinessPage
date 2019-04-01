84ee705eec6191dfa9a79b70c742a299909845aa
Adrian Mostowski <adaxelx@o2.pl>
2019-04-01 23:04:00 +0200

    add animation to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,33 +2,70 @@ import React from 'react';
 
 import '../css/Footer.css'
 
-const Footer = () => {
-    return ( 
-        <>
-        <footer>
-            <p className="title">Więcej o nas!</p>
-            <div className="socials">
-                <p>Instagram</p> 
-                <p>Instagram</p> 
-            </div>
-            <div className="contactFot">
-                <p className='header'>Skontaktuj się z nami!</p>
-                <p>exapsaka@o2.pl</p> 
-                <p>530 439 439</p>
-                <p>Lub napisz wiadomość tutaj!</p>
-            </div>
-            <div className="partnership">
-                <p className='header'>Dołącz do nas!</p>
-                <p>work.saodjk@o2.pl</p>
-            </div>
-            <div className="placeFot">
-                <p className='header'>Znajdziesz nas pod adresem:</p>
-                <p>aosdk sapodasd 23, alskd laskda</p>
-            </div>
-        </footer>
-        </>
+let off;
+let height;
+let windowH = window.innerHeight;
+
+class Footer extends React.Component{
+
+    state = {
+        active: false,
+    }
+
+    handleScroll = () =>{
+        let scrollV = window.scrollY;
+            off = this.footer.current.offsetTop
+            height = this.footer.current.offsetHeight;
+            if(off<scrollV+windowH-height/2){
+                this.setState({
+                    active: true,
+                })
+            }
+            else{
+                this.setState({
+                    active: false,
+                })
+            }
+        }
         
-     );
+    
+        footer = React.createRef();
+    
+        componentDidMount() {
+            window.addEventListener('scroll', this.handleScroll);
+        }
+        componentWillUnmount() {
+            window.removeEventListener('scroll', this.handleScroll);
+          }
+    render(){
+        return ( 
+            <>
+            <footer ref={this.footer} className={this.state.active ? 'active' : ''}>
+                <p className="title">Więcej o nas!</p>
+                <div className="socials">
+                    <p>Instagram</p> 
+                    <p>Instagram</p> 
+                </div>
+                <div className="contactFot">
+                    <p className='header'>Skontaktuj się z nami!</p>
+                    <p>exapsaka@o2.pl</p> 
+                    <p>530 439 439</p>
+                    <p>Lub napisz wiadomość tutaj!</p>
+                </div>
+                <div className="partnership">
+                    <p className='header'>Dołącz do nas!</p>
+                    <p>work.saodjk@o2.pl</p>
+                </div>
+                <div className="placeFot">
+                    <p className='header'>Znajdziesz nas pod adresem:</p>
+                    <p>aosdk sapodasd 23, alskd laskda</p>
+                </div>
+            </footer>
+            </>
+            
+         );
+    }
+   
 }
  
 export default Footer;
