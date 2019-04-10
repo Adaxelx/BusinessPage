d5ea05a648716cdf1ff1c00b8f7f7d742c45ad9c
Adrian Mostowski <adaxelx@o2.pl>
2019-04-10 21:38:27 +0200

    working with sticky scrolling

--- src/components/OfferComputer.js
+++ src/components/OfferComputer.js
@@ -6,29 +6,76 @@ import img from '../img/tower.jpg'
 import img2 from '../img/buildings.jpg'
 import img3 from '../img/clouds.jpg'
 import img4 from '../img/hong-kong.jpg'
-let off;
-let height;
-let windowH = window.innerHeight;
+let conOffs = [];
+let conHeights = [];
+// let txtOffs = [];
+let txtHeights = [];
 
 class OfferComputer extends React.Component {
+    text1 = React.createRef();
+    text2 = React.createRef();
+    container1 = React.createRef();
+    container2 = React.createRef();
+    texts = [this.text1,this.text2]
+    moveText = () =>{
+        const scrollValue = window.scrollY;
+        this.texts.forEach((txt,i) => {
+            const value = conHeights[i] + conOffs[i] - txtHeights[i]-30;
 
+            if(conOffs[i]<scrollValue && value>scrollValue){
+                txt.current.style.position = 'fixed';
+                txt.current.style.width = 50 + '%';
+                txt.current.style.top = 20 +'px';
+            
+            }else if(value<=scrollValue){
+                txt.current.style.position = 'absolute';
+                txt.current.style.top = conHeights[i] - txtHeights[i] -20 + 'px';
+                txt.current.style.width = 100 + '%'
+         
+            }
+            else{
+                txt.current.style.position = 'absolute';
+                txt.current.style.top = 20 + 'px';
+                txt.current.style.width = 100 + '%'
+               
+            }
+        })
+    }
+
+    componentDidMount() {
+        const containers = [this.container1,this.container2]
+        this.texts.forEach(txt =>{
+            txtHeights.push(txt.current.offsetHeight)
+            // txtOffs.push(txt.current.getBoundingClientRect().top)
+        })
+        containers.forEach(con => {
+            conHeights.push(con.current.offsetHeight)
+            conOffs.push(con.current.getBoundingClientRect().top)
+        })
+        window.addEventListener('scroll', this.moveText)
+    }
+    componentWillUnmount(){
+        window.removeEventListener('scroll', this.moveText)
+    }
 
     render() { 
         return ( 
             <section className='offerComputer'>
                 <p className='title' data-text="Oferta">Oferta</p>
                 <section className="articles">
-                <article>
-                    <i className="fas fa-users icon"></i>
+                <article ref={this.container1}>
+                    <div className='scrollItems' ref={this.text1}>
                     <p className="titleArt">Lorem ipsum</p>
                     <p className="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam alias maxime illum omnis praesentium cum delectus laboriosam. Repellendus non officia quaerat rerum culpa debitis, dolorem sed perferendis officiis corrupti minus!
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id ducimus perspiciatis, provident at quod mollitia accusamus eligendi, quia facere, accusantium blanditiis. Atque, in praesentium! Veniam facere ipsam facilis harum aliquid? </p>
+                    </div>
                 </article>
-                <article>
-                    <i className="fas fa-users icon"></i>
+                <article ref={this.container2}>
+                <div className='scrollItems' ref={this.text2}>
                     <p className="titleArt">Lorem ipsum</p>
                     <p className="content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam alias maxime illum omnis praesentium cum delectus laboriosam. Repellendus non officia quaerat rerum culpa debitis, dolorem sed perferendis officiis corrupti minus!
                     Lorem, ipsum dolor sit amet consectetur adipisicing elit. Id ducimus perspiciatis, provident at quod mollitia accusamus eligendi, quia facere, accusantium blanditiis. Atque, in praesentium! Veniam facere ipsam facilis harum aliquid? </p>
+                    </div>
                 </article>
                 </section>
                 <section className="images">
